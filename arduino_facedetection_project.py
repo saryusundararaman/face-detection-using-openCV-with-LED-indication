2e3b84031c2e3a87a83ce0e9a31bc58c88cd3ef6
Saryu Sundararaman <121036030+saryusundararaman@users.noreply.github.com>
2023-03-25 18:47:54 +0530

    Update arduino_facedetection_project.py

--- arduino_facedetection_project.py
+++ arduino_facedetection_project.py
@@ -2,20 +2,20 @@ import cv2
 from cvzone.FaceDetectionModule import FaceDetector
 from cvzone.SerialModule import SerialObject
 
-c = cv2.VideoCapture(0)
+c = cv2.VideoCapture(0) #0 for inbuilt camera
 detector = FaceDetector()
-ard = SerialObject()
+ard = SerialObject() #serial object indicates the port to which arduino uno is connected
 
 while True:
     success, img = c.read()
     img, bBxoes = detector.findFaces(img) 
 
     if bBxoes:
-        ard.sendData([1,0])
+        ard.sendData([1,0]) #if a face is detected, green LED glows and red LED does not glow
     else:
-        ard.sendData([0,1])
+        ard.sendData([0,1]) #if a face is not detected red LED glows and green doesn't
 
-    if cv2.waitKey(1) & 0xFF == ord('d'):
+    if cv2.waitKey(1) & 0xFF == ord('d'): #pressing the key 'd' closes the webcam
         break
 
     cv2.imshow("Video", img)
