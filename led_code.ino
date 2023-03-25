4941f32c8386ba6e5b7d29774c611e9aaa39dd08
Saryu Sundararaman <121036030+saryusundararaman@users.noreply.github.com>
2023-03-25 18:44:24 +0530

    Update led_code.ino

--- led_code.ino
+++ led_code.ino
@@ -3,9 +3,9 @@
 SerialData serialData(2, 1); //(numOfValsRec,digitsPerValRec)
 int valsRec[2]; // array of int with size numOfValsRec 
 
-void setup() {
-  pinMode(8, OUTPUT);
-  pinMode(13, OUTPUT);
+void setup() { 
+  pinMode(8, OUTPUT); //pin for LED indicating that a face has been detected (green LED)
+  pinMode(13, OUTPUT); // pin for LED indicating that a face has been detected (red LED)
   serialData.begin();
 }
 
